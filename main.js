9f9cb08db6126e16e283fecd5d14faa4af3241c3
cipepser <respepic@gmail.com>
2018-10-13 15:44:15 +0900

    add main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+
+function listupfiles() {
+  const options = {
+    ts_to: elapsedDaysToUnixTime(100),
+    count: 1000
+  }
+  Logger.log(filesList(options))
+}
+
+
+/* ファイルのリスト */
+function filesList(data){
+  const SLACK_ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty("TOKEN");
+  const params = {
+    'token': SLACK_ACCESS_TOKEN,
+    'ts_to': data.ts_to,
+    'count': data.count
+  }
+  const options = {
+    'method': 'POST',
+    'payload': params
+  }
+  const res =  UrlFetchApp.fetch('https://slack.com/api/files.list',options);
+  return JSON.parse(res.getContentText());
+}
+
+function elapsedDaysToUnixTime(sec){
+  const date = new Date();
+  const now = Math.floor(date.getTime()/ 1000); // unixtime[sec]
+  return now - sec + '';
+  // return now - 8.64e4 * days + '' // 8.64e4[sec] = 1[day] 文字列じゃないと動かないので型変換している
+}
+
+function sample() {
+  
+}
